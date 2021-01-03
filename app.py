451138613a8e3949606bd3c5cf167b21e4345e7e
John George <Sci.JohnGeorge.2017@alexu.edu.eg>
2021-01-03 12:52:58 +0200

    b4

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ class Person:
 	def __init__(self):
 		self.__title = "ABC"
 	def set_name(self, name: str):
-		self.__name = name
+		self.__name = f"Emp: {name}"
 
 	def set_age(self, age: int):
 		self.__age = age
